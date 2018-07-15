b25df65dab8943205577f788124f5a334573d119
loxstomper <lochieashcroft@gmail.com>
2018-07-15 15:29:54 +1000

    prefix addition now works correctly

---
+++ week-2/exercise-4/prefixadd.h
@@ -0,0 +1,6 @@
+#ifndef PREFIXADD_H
+#define PREFIXADD_H
+
+int process(int* curr_index, int last_index, char *argv[]);
+
+#endif
---
+++ week-2/exercise-4/process.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdlib.h>
+
+int process(int *curr_index, int last_index, char *argv[])
+{
+    if (*curr_index > last_index) return 1;
+
+    if (strlen(argv[*curr_index]) == 1 && *(argv[*curr_index]) == '+')
+    {
+        (*curr_index) ++;
+        int operand1 = process(curr_index, last_index, argv);
+        (*curr_index) ++;
+        int operand2 = process(curr_index, last_index, argv);
+
+        return operand1 + operand2;
+    }
+    else
+    {
+        int result = atoi(argv[*(curr_index)++]);
+        return result;
+    }
+}
