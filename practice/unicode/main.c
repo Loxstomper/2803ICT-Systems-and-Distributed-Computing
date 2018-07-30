0428cb330239e77e5ff88e5b9644e62b4d8fa558
loxstomper <lochieashcroft@gmail.com>
2018-07-30 10:34:11 +1000

    made program to read in unicode file and print contents

--- practice/unicode/main.c
+++ practice/unicode/main.c
@@ -1,4 +1,25 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <stdlib.h>
+#include <locale.h>
+
 int main(int argc, char ** argv)
 {
+    setlocale(LC_ALL, "en_AU");
+
+    FILE* in;
+
+    wint_t c;
+
+    in = fopen("unicode.txt", "r");
+
+    while ((c = fgetwc(in)) != WEOF)
+    {
+        printf("%lc", c);
+    }
+    
+    printf("\n");
+
+
     return 0;
 }
