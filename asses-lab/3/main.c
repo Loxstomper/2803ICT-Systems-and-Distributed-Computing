d00169d76de76578c3588cc159d520be6c9ad41a
loxstomper <lochieashcroft@gmail.com>
2018-09-03 11:50:37 +1000

    coding alpha prefix calculator didnt work, going to use geek for geeks

--- asses-lab/3/main.c
+++ asses-lab/3/main.c
@@ -5,10 +5,100 @@
 #include <signal.h>
 #include <sys/time.h>
 #include <string.h>
+#include <math.h>
 
 #define BUFFER_SIZE 1024
 
-/* just fork once, no zombies, parent will loop a lot */
+
+
+// --------------------------------- PREFIX STUFF ------------------------------------- //
+#define LIMIT 500
+ 
+typedef struct prefix_expression
+{
+      int top;
+      int stack[LIMIT];
+}stack_structure;
+ 
+void initialize(stack_structure *temp)
+{
+      temp->top = -1;
+}
+ 
+void push(stack_structure *temp, int num)
+{
+      if(temp->top == LIMIT)
+      {
+            printf("\nStack Overflow\n");
+            exit(-1);
+      }
+      else
+      {
+            temp->top++;
+            temp->stack[temp->top]=num;
+      }
+}
+ 
+int pop(stack_structure *temp)
+{
+      int num;
+      if(temp->top < 0)
+      {
+            printf("\nStack Underflow\n");
+            exit(-1);
+      }
+      num = temp->stack[temp->top];
+      temp->top--;
+      return num;
+}
+ 
+int top(stack_structure *temp)
+{
+      return temp->stack[0];
+}
+ 
+void evaluate_prefix(stack_structure *temp, char operator, int x, int y)
+{
+      int result;
+      switch(operator)
+      {
+            case '-': result = x - y;
+                      break;
+            case '*': result = x * y;
+                      break;
+            case '%': result = x % y;
+                      break;
+            case '/': result = x / y;
+                      break;
+            case '+': result = x + y;
+                      break;
+            case '$': result = pow(x, y);
+                      break;
+      }
+      push(temp, result);
+}
+ 
+int find_operator(char ch)
+{
+      switch(ch)
+      {
+            case '%': return 20;
+            case '/': return 20; 
+            case '*': return 20;
+            case '-': return 20;
+            case '$': return 20;
+            case '+': return 20;
+            default : return 10;
+      }
+}
+ 
+
+
+
+
+
+// --------------- MAIN STUFF ------------------ //
+
 
 int main(int argc, char** argv)
 {
@@ -32,9 +122,15 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    // create the child
+
+    // +++++++++++++ FORK ++++++++++++ //
     if ((child_pid = fork()) == 0)
     {
+        int x, y, element, length, count, result;
+        stack_structure expression_stack;
+        initialize(&expression_stack);
+        char output[BUFFER_SIZE];
+
         while (1)
         {
             close(p1[1]);
@@ -47,7 +143,36 @@ int main(int argc, char** argv)
             }
             else
             {
-                if (write(p2[1], "im the child\n", sizeof("im the child\n") + 1) == -1)
+                // ============= PREFIX =========== //
+                length = strlen(buffer);
+
+                for(count = length - 1; count >= 0; count--)
+                {
+                    if(buffer[count] == '\0' || buffer[count] == ' ')
+                    {
+                        continue;
+                    }
+                    switch(find_operator(buffer[count]))
+                    {
+                        case 20 :   x = pop(&expression_stack);
+                                    y = pop(&expression_stack);
+                                    evaluate_prefix(&expression_stack, buffer[count], x, y);
+                                    break;
+                        case 10 : element = buffer[count] - '0';
+                                    push(&expression_stack, element);
+                                    break;
+                    }
+                }
+
+                result = top(&expression_stack);
+
+
+                sprintf(output, "Result: %d", result);
+                printf("CHILD CALCULATED: [%d]\n", result);
+
+                // ==================================== //
+
+                if (write(p2[1], output, sizeof(output) + 1) == -1)
                 {
                     perror("write() error in child");
                     exit(1);
@@ -56,20 +181,11 @@ int main(int argc, char** argv)
         }
 
         exit(0);
-
-        // while (1)
-        // {
-            // wait to read the pipe
-            // perform evaluation
-            // write to the pipe
-        // }
-
     }
     else
     {
         while (1)
         {
-            printf("im the parent\n");
             close(p1[0]);
             close(p2[1]);
 
@@ -77,7 +193,7 @@ int main(int argc, char** argv)
 
             fgets(buffer, BUFFER_SIZE, stdin);
 
-            printf("Parenting writing to pipe1\n");
+            // printf("Parenting writing to pipe1\n");
             if (write(p1[1], buffer, strlen(buffer) + 1) == -1)
             {
                 perror("write() error in parent");
@@ -90,17 +206,10 @@ int main(int argc, char** argv)
             }
             else
             {
-                printf("Parent read: %s\n", buffer);
+                printf("Result: %s\n", buffer);
             }
         }
-
-        // write to the pipe
-        // print from the pipe output
     }
-
-
-
-    
 
     return 0;
 }
